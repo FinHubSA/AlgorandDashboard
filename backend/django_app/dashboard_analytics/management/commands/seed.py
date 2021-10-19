46902b18a4b9792e951fb379adf8f95f528830a7
Alex Stewart <astewart1510@gmail.com>
2021-10-19 14:46:59 +0200

    Merge with main

--- backend/django_app/dashboard_analytics/management/commands/seed.py
+++ backend/django_app/dashboard_analytics/management/commands/seed.py
@@ -1,17 +1,28 @@
 from django.core.management.base import BaseCommand, CommandError
 import requests
+from django.db import models
 from dashboard_analytics.models import AccountType, InstrumentType, Account, Transaction
+import json
+from algosdk.v2client import indexer
+import pandas as pd
 
 
 # file to indexer API and save into postgres database
 # Indexer port configuration
-INDEXER_PORT = "123"
-# bla bla bla
+ALGOD_ADDRESS = "http://localhost:4001"
+ALGOD_TOKEN = "a" * 64
+INDEXER_ADDRESS = "http://localhost:8980"
+INDEXER_TOKEN = ALGOD_TOKEN
+indexer_client = indexer.IndexerClient(INDEXER_TOKEN, INDEXER_ADDRESS)
+
+#print("Transaction search: " + json.dumps(transactions, indent=2, sort_keys=True))
 
 
-def get_data():
+def get_accounts():
     print("Begin grabbing data from indexer API")
-    # code below
+    # grab all accounts
+    accounts = indexer_client.accounts()
+    #
 
 
 def seed_data():
@@ -23,6 +34,12 @@ class Command(BaseCommand):
     help = "Seed database with Indexer API"
 
     def handle(self, *args, **options):
-        self.stdout.write("My sample command just ran.")  # NEW
+        indexer_client = indexer.IndexerClient(INDEXER_TOKEN, INDEXER_ADDRESS)
+        transactions = indexer_client.search_transactions()
+        accounts = indexer_client.accounts()
+        self.stdout.write("Accounts" + json.dumps(accounts,
+                          indent=2, sort_keys=True))  # NEW
         # seed_data()
+        self.stdout.write("Transactions" + json.dumps(transactions,
+                          indent=2, sort_keys=True))
         self.stdout.write("completed")
